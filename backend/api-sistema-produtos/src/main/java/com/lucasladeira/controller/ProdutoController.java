5da9dc1d1b2cfbacb8e5ebd793085b4332e6bb38
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-13 18:04:07 -0300

    findById Produto controller

--- backend/api-sistema-produtos/src/main/java/com/lucasladeira/controller/ProdutoController.java
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class ProdutoController {
 	public ResponseEntity<List<Produto>> getAll(){
 		List<Produto> list = produtoService.getAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Integer id){
+		Produto produto = produtoService.getById(id);
+		return ResponseEntity.ok().body(produto);
 	}
 	
 }
