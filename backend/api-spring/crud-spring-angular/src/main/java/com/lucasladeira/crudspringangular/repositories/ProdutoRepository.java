52e2044c8e86379191e7a48d639503f87f1a56b9
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-13 16:46:52 -0300

    refazendo back-end do 0

--- backend/api-spring/crud-spring-angular/src/main/java/com/lucasladeira/crudspringangular/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lucasladeira.crudspringangular.repositories;
-
-import com.lucasladeira.crudspringangular.entities.Produto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
-    
-}
