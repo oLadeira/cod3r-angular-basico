90e66691c0857e0f90cbda09a20cc3e88d94d27a
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-14 15:57:19 -0300

    entidade Fornecedor

---
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/entities/Fornecedor.java
@@ -0,0 +1,33 @@
+package com.lucasladeira.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Fornecedor implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String tradingName;
+	private String cnpj;
+		
+	
+}
