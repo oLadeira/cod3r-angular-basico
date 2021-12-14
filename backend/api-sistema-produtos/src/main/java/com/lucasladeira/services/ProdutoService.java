76b8af859cf858daa4a2ba27c754172b832d3860
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-14 15:01:53 -0300

    update Produto implementacao & PUT controller

--- backend/api-sistema-produtos/src/main/java/com/lucasladeira/services/ProdutoService.java
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/services/ProdutoService.java
@@ -15,12 +15,13 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	
+	//GET
 	public List<Produto> getAll(){
 		List<Produto> list = produtoRepository.findAll();
 		return list;
 	}
 	
+	//GET
 	public Produto getById(Integer id) {
 		Optional<Produto> opt = produtoRepository.findById(id);
 		
@@ -31,9 +32,22 @@ public class ProdutoService {
 		return opt.get();
 	}
 	
+	//POST
 	public Produto save (Produto produto) {
+		produto.setId(null);
 		produto = produtoRepository.save(produto);
 		return produto;
+	}
+	
+	//PUT
+	public void update (Integer id, Produto produto) {
+		Optional<Produto> opt = produtoRepository.findById(id);
+		
+		if (opt.isEmpty()) {
+			//tratar erro
+		}
+		produto.setId(id);
+		produtoRepository.save(produto);
 	}
 	
 }
