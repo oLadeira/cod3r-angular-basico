9fe5ef4070fd7b2e65205afa64876313019251ab
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-14 15:58:38 -0300

    FornecedorRepository

---
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/repositories/FornecedorRepository.java
@@ -0,0 +1,9 @@
+package com.lucasladeira.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucasladeira.entities.Fornecedor;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>{
+
+}
