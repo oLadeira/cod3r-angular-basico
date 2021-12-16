0bba8ba13c393c9820bab356b03424849712399d
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-16 17:33:42 -0300

    backend - tratando excecoes DataIntegrityException de Fornecedor

--- backend/api-sistema-produtos/src/main/java/com/lucasladeira/controller/exceptions/ResourceExceptionHandler.java
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/controller/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.lucasladeira.services.exceptions.DataIntegrityException;
 import com.lucasladeira.services.exceptions.EntityNotFoundException;
 
 @ControllerAdvice
@@ -23,6 +24,17 @@ public class ResourceExceptionHandler {
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardErrorBody> dataIntegrityViolation(DataIntegrityException e, HttpServletRequest request){
+		StandardErrorBody err = new StandardErrorBody();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError("Data Integrity Violation");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
 }
--- backend/api-sistema-produtos/src/main/java/com/lucasladeira/services/SupplierService.java
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/services/SupplierService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lucasladeira.entities.Supplier;
 import com.lucasladeira.repositories.SupplierRepository;
+import com.lucasladeira.services.exceptions.DataIntegrityException;
 import com.lucasladeira.services.exceptions.EntityNotFoundException;
 
 @Service
@@ -54,8 +56,12 @@ public class SupplierService {
 		Optional<Supplier> opt = supplierRepository.findById(id);
 		
 		opt.orElseThrow(() -> new EntityNotFoundException("Fornecedor não encontrado!, ID: " + id));
-				
-		supplierRepository.deleteById(id);
+		
+		try {
+			supplierRepository.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível deletar o Fornecedor, há produtos relacionados a ele");
+		}		
 	}
 	
 	
