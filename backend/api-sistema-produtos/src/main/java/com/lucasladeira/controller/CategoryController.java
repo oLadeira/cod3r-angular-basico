9ffdcd871897073204b1adb0ba068de861d3fd5b
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-17 15:47:11 -0300

    :sparkles: feat:  Service save Categoria implementado, metodo POST Controller

--- backend/api-sistema-produtos/src/main/java/com/lucasladeira/controller/CategoryController.java
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +36,7 @@ public class CategoryController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> save(CategoryNewDTO categoryNewDTO){
+	public ResponseEntity<Void> save(@RequestBody CategoryNewDTO categoryNewDTO){
 		categoryService.save(categoryNewDTO);
 		return ResponseEntity.ok().build();
 	}
