069e9362d1a330a450a64eafd87365b621d38440
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-20 10:09:57 -0300

    :goal_net: exception: tratando campos vazios & min e max de caracteres ProductNewDTO e CategoryNewDTO

--- backend/api-sistema-produtos/src/main/java/com/lucasladeira/dto/CategoryNewDTO.java
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/dto/CategoryNewDTO.java
@@ -2,6 +2,9 @@ package com.lucasladeira.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +17,9 @@ public class CategoryNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@Max(20)
+	@NotBlank(message = "Campo não informado")
 	private String name;
 	
 	
--- backend/api-sistema-produtos/src/main/java/com/lucasladeira/dto/ProductNewDTO.java
+++ backend/api-sistema-produtos/src/main/java/com/lucasladeira/dto/ProductNewDTO.java
@@ -2,6 +2,8 @@ package com.lucasladeira.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 import lombok.Getter;
@@ -15,12 +17,15 @@ public class ProductNewDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	@Min(3)
+	@Max(70)
 	@NotBlank(message = "Campo não informado")
 	private String name;
 	
-	
+	@NotBlank(message = "Campo não informado")
 	private Double price;
 	
+	@NotBlank(message = "Campo não informado")
 	private Integer supplier;
 		
 
