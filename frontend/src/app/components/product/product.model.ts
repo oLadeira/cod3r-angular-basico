df1027c29c7451a65464fcef4a461b61326927ce
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-12-23 11:27:50 -0300

    :lipstick: style:  flex-direction column form atualizar Fornecedor

--- frontend/src/app/components/product/product.model.ts
+++ frontend/src/app/components/product/product.model.ts
@@ -1,6 +1,8 @@
+import { Supplier } from './../supplier/supplier.model';
 export interface Product{
     id?: number,
     name: string,
     price?: number,
     supplier?: number
+    //supplier: Supplier
 }
